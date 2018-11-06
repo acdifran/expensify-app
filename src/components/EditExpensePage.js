d273d9751c35bda7b94213214dba3d653495b762
Anthony DiFrancesco <acdifran@gmail.com>
2018-11-06 13:22:37 -0500

    fix quotes

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -40,13 +40,9 @@ export class EditExpensePage extends React.Component {
   }
 }
 
-const mapStateToProps = (state, props) => {
-  return {
-    expense: state.expenses.find(
-      expense => expense.id === props.match.params.id
-    )
-  };
-};
+const mapStateToProps = (state, props) => ({
+  expense: state.expenses.find(expense => expense.id === props.match.params.id)
+});
 
 const mapDispatchToProps = dispatch => ({
   startEditExpense: (id, expense) => dispatch(startEditExpense(id, expense)),
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,29 @@
-const path = require("path");
-const webpack = require("webpack");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-if (process.env.NODE_ENV === "test") {
-  require("dotenv").config({ path: ".env.test" });
-} else if (process.env.NODE_ENV === "development") {
-  require("dotenv").config({ path: ".env.development" });
+if (process.env.NODE_ENV === 'test') {
+  require('dotenv').config({ path: '.env.test' }); // eslint-disable-line global-require
+} else if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config({ path: '.env.development' }); // eslint-disable-line global-require
 }
 
 module.exports = env => {
-  const isProduction = env === "production";
+  const isProduction = env === 'production';
 
   return {
-    entry: ["@babel/polyfill", "./src/app.js"],
+    entry: ['@babel/polyfill', './src/app.js'],
     output: {
-      path: path.join(__dirname, "public", "dist"),
-      filename: "bundle.js"
+      path: path.join(__dirname, 'public', 'dist'),
+      filename: 'bundle.js'
     },
-    mode: "development",
+    mode: 'development',
     module: {
       rules: [
         {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           test: /\.js$/,
           exclude: /node_modules/
         },
@@ -32,13 +32,13 @@ module.exports = env => {
           use: [
             MiniCssExtractPlugin.loader,
             {
-              loader: "css-loader",
+              loader: 'css-loader',
               options: {
                 sourceMap: true
               }
             },
             {
-              loader: "sass-loader",
+              loader: 'sass-loader',
               options: {
                 sourceMap: true
               }
@@ -49,34 +49,32 @@ module.exports = env => {
     },
     plugins: [
       new MiniCssExtractPlugin({
-        filename: "styles.css"
+        filename: 'styles.css'
       }),
       new webpack.DefinePlugin({
-        "process.env.FIREBASE_API_KEY": JSON.stringify(
-          process.env.FIREBASE_API_KEY
-        ),
-        "process.env.FIREBASE_AUTH_DOMAIN": JSON.stringify(
+        'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(
           process.env.FIREBASE_AUTH_DOMAIN
         ),
-        "process.env.FIREBASE_DATABASE_URL": JSON.stringify(
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(
           process.env.FIREBASE_DATABASE_URL
         ),
-        "process.env.FIREBASE_PROJECT_ID": JSON.stringify(
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(
           process.env.FIREBASE_PROJECT_ID
         ),
-        "process.env.FIREBASE_STORAGE_BUCKET": JSON.stringify(
+        'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(
           process.env.FIREBASE_STORAGE_BUCKET
         ),
-        "process.env.FIREBASE_MESSAGING_SENDER_ID": JSON.stringify(
+        'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.stringify(
           process.env.FIREBASE_MESSAGING_SENDER_ID
         )
       })
     ],
-    devtool: isProduction ? "source-map" : "inline-source-map",
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
-      contentBase: path.join(__dirname, "public"),
+      contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
-      publicPath: "/dist/"
+      publicPath: '/dist/'
     }
   };
 };
